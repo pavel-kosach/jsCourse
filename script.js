f4c05c306ff6c059670b817f48e6d94228edeb66
Pavel Kosach <kosachdev@gmail.com>
2023-01-30 21:15:41 +0300

    Выполнено задание к 3 уроку

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let title = "Название проекта";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 10;
-let rollback = 10;
-let fullPrice = 1000000;
-let adaptive = true;
+"use strict";
+
+let title = prompt("Как называется ваш проект?", "Мой проект");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные"); 
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
+let rollback = 5;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?", "service1"); 
+let servicePrice1 = +prompt("Сколько это будет стоить?", "500");
+let service2 = prompt("Какой дополнительный тип услуги нужен?", "service2");
+let servicePrice2 = +prompt("Сколько это будет стоить?", "1000");
+
+// Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг (screenPrice + servicePrice1 + servicePrice2)
+// и результат занести в переменную fullPrice
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+console.log(servicePercentPrice);
 
 // Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 console.log(typeof title);
@@ -13,21 +26,42 @@ console.log(typeof adaptive);
 // Вывести в консоль длину строки из переменной screens
 console.log(screens.length);
 
-// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
-console.log(
-  "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани"
-);
-console.log(
-  "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани"
-);
+// Вывести в консоль Стоимость верстки экранов (screenPrice) рублей и Стоимость разработки сайта
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
 
 // Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
 console.log(screens.toLowerCase().split());
-// screens = screens.toLowerCase();
-// screens = screens.split();
-// console.log(screens);
 
 // Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-console.log(
-  "Процент отката посреднику за работу " + fullPrice * (rollback / 100)
-);
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100));
+
+// Первый вариант решения через if else
+// fullPrice = 15000;
+// if (fullPrice > 30000 || fullPrice == 30000) {
+//   alert("Даем скидку в 10%");
+// } else if (fullPrice > 15000 && fullPrice < 30000) {
+//   alert("Даем скидку в 5%");
+// } else if (fullPrice < 15000 && fullPrice > 0 || fullPrice == 15000) {
+//   alert("Скидка не предусмотрена");
+// } else if  (fullPrice < 0 || fullPrice == 0) {
+//   alert("Что то пошло не так");
+// }
+
+// Второй вариант решения через switch
+switch (true) {
+  case fullPrice > 30000 || fullPrice == 30000:
+    alert("Даем скидку в 10%");
+    break;
+    case fullPrice > 15000 && fullPrice < 30000:
+      alert("Даем скидку в 5%");
+      break;
+    case fullPrice < 15000 && fullPrice > 0 || fullPrice == 15000:
+      alert("Скидка не предусмотрена");
+      break;
+    case fullPrice < 0 || fullPrice == 0:
+      alert("Что то пошло не так");
+      break;
+    default:
+      alert("Что-то пошло не так");
+}
