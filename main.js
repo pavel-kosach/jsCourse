a46a643a81d5828a4f1da06510ccb8de74860e0c
Pavel Kosach <kosachdev@gmail.com>
2023-02-21 15:46:50 +0300

    add les16 task

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 "use strict";
 // Напишите расширения метода прототипа:
 // 1) Два класса, First и Second, Second наследует от First .
-
-// 2) В First есть метод hello - он печатает в консоль "Привет я метод родителя!".
+class First {
+  // 2) В First есть метод hello - он печатает в консоль "Привет я метод родителя!".
+  hello() {
+    console.log(`Привет я метод родителя!`);
+  }
+}
 
 // 3) Нужно написать в Second метод hello, чтоб он сначала вызывал метод hello из First, а потом сразу печатал в консоль "А я наследуемый метод!"
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log(`А я наследуемый метод!`);
+  }
+}
+
+const start = new Second();
+start.hello();
