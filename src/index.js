6af429a6d8900fc0241f6056a3f294245b5b56fd
Pavel Kosach <kosachdev@gmail.com>
2023-02-28 17:29:19 +0300

    add main les21 task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import calc from "./modules/calc";
 
 timer("28 february 2023");
 menu();
 modal();
+calc();
