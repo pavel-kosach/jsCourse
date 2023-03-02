ab031c2c6b71da88866cada352500a3e84e46134
Pavel Kosach <kosachdev@gmail.com>
2023-03-03 00:03:39 +0300

    add les22 from video

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import calc from "./modules/calc";
+import tabs from "./modules/tabs";
 
-timer("28 february 2023");
+timer("10 march 2023");
 menu();
 modal();
 calc();
+tabs();
